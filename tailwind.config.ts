7074b05dc43187f0c27ff30f6a43825f9aa2b10e
AmanRai <aman.rai@buzzworks.in>
2024-12-06 01:11:47 +0530

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: [
@@ -8,6 +9,20 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: '1.5rem',
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '100%',
+          xl: '1200px',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...fontFamily.mono],
+      },
       colors: {
         // Main brownish colors
         accent: '#6F4F1E', // Primary brown accent color
